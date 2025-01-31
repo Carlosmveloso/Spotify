e2d533f2bb3406fe0ae091780a4e58521cd97c19
Carlos Eduardo <carloseduardomveloso@gmail.com>
2025-01-30 21:39:44 -0800

    começando a parte de javascript do projeto

---
+++ script.js
@@ -0,0 +1,31 @@
+const searchInput = document.getElementById("search-input");
+const resultArtist = document.getElementById("result-artist");
+const resultPLaylist = document.getElementById("result-playlists");
+
+function requestAPI(searchTerm) {
+  const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((result) => displayResults(result));
+}
+
+function displayResults(result) {
+  resultPLaylist.classList.add("hidden");
+  const artistName = docuement.getElementById("artist-name");
+  const artistImage = document.getElementById("artist-img");
+  result.forEach(element => {
+    artistName.innerText = element.name;
+    artistImage.src = element.urlImg;
+  });
+  result.classList.remove("hidden");
+}
+
+document.addEventListener("input", () => {
+  const searchValue = searchInput.value.toLowerCase();
+  // Um = é igual a concatenação, Dois == significa igualdade, Três === significa igualdade e do mesmo
+  if (searchValue === "") {
+    resultPLaylist.classList.add("hidden");
+    resultArtist.classList.remove("hidden");
+    return;
+  }
+});
